369ec6701bb955980b23538895675cf1e8251899
Alexey Veprev <aveprev27@gmail.com>
2022-05-05 23:08:34 +0300

    Use images from GitHub Packages (#4)

---
+++ jenkins/build_and_push_images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+USER=$1
+TOKEN=$2
+echo ${TOKEN} | docker login ghcr.io -u ${USER} --password-stdin
+
+set -e -x
+
+function preare_image() {
+    DOCKERFILE=$1
+    IMAGE_TAG=$2
+
+    docker build -f ${SCRIPTPATH}/${DOCKERFILE} -t ghcr.io/${USER}/jenkins_demo_project/${IMAGE_TAG} .
+    docker push ghcr.io/${USER}/jenkins_demo_project/${IMAGE_TAG}
+    docker rmi ghcr.io/${USER}/jenkins_demo_project/${IMAGE_TAG}
+}
+
+preare_image Dockerfile.builder     demo_builder:latest
+preare_image Dockerfile.tester_deb  demo_tester:deb
+preare_image Dockerfile.tester_ub18 demo_tester:ub18
+preare_image Dockerfile.tester_ub20 demo_tester:ub20
+preare_image Dockerfile.tester_ub22 demo_tester:ub22
+
+docker logout
